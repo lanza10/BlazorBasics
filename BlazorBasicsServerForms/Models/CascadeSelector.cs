570e226ebbe4f6d2d3ac08cbb4f3cdade9ed7684
javierln <javier.lanza@semicrol.com>
2024-08-12 11:29:27 +0200

    Cascade Selector

---
+++ BlazorBasicsServerForms/Models/CascadeSelector.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorBasicsServerForms.Models
+{
+    public class CascadeSelector
+    {
+        public int Selector1 { get; set; }
+        public int Selector2 { get; set; }
+    }
+}
