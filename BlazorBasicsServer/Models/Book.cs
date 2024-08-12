b15f68cf715f89757a790f600a1cdb0461558e3b
javierln <javier.lanza@semicrol.com>
2024-08-12 07:50:13 +0200

    One way data binding

---
+++ BlazorBasicsServer/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorBasicsServer.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Active { get; set; }
+        public int Pages { get; set; } 
+    }
+}
