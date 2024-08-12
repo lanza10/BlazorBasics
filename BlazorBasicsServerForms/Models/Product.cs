c8a72765791d662ebc227ed2f11fb1e5bb6962f6
javierln <javier.lanza@semicrol.com>
2024-08-12 10:03:47 +0200

    Validations

--- BlazorBasicsServerForms/Models/Product.cs
+++ BlazorBasicsServerForms/Models/Product.cs
@@ -1,8 +1,18 @@
-﻿namespace BlazorBasicsServerForms.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBasicsServerForms.Models
 {
     public class Product
     {
+        [Required(ErrorMessage = "The Name of the product is mandatory")]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "Name must have between 5 and 10 chars")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "The Description of the product is mandatory")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Description must have between 5 and 100 chars")]
         public string Description { get; set; }
+        [Range(1,20, ErrorMessage = "Amount must be a number between 1 and 20")]
+        public int Amount { get; set; }
+        [Range(typeof(bool),"true","true", ErrorMessage = "Product must be Active")]
+        public bool Active { get; set; }
     }
 }
