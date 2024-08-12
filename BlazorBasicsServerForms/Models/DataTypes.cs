a340cf1a1488c6f354787e4797596c68660f6c52
javierln <javier.lanza@semicrol.com>
2024-08-12 11:06:09 +0200

    Advanced Forms

---
+++ BlazorBasicsServerForms/Models/DataTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorBasicsServerForms.Models
+{
+    public class DataTypes
+    {
+        public string StringProp { get; set; }
+
+        public string PasswordProp { get; set; }
+
+        public int IntProp { get; set; }
+
+        public double DoubleProp { get; set; }
+        public bool BoolProp { get; set; }
+
+        public DateTime DateTimeProp { get; set; }
+
+        public string DropdownListProp { get; set; }
+    }
+}
