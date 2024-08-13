68faa494b3bbaa439ada5f44b3079f5400517146
javierln <javier.lanza@semicrol.com>
2024-08-13 15:46:07 +0200

    Book list implemented

--- BlazorCRUD/Program.cs
+++ BlazorCRUD/Program.cs
@@ -1,4 +1,5 @@
 using BlazorCRUD.Data;
+using BlazorCRUD.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+//Add repository on dependency injection
+builder.Services.AddScoped<IRepository, Repository>();
 
 var app = builder.Build();
 
