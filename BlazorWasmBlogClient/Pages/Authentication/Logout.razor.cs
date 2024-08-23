7c2955c1c7905f7be6ca47ccfa8f09fe0c11fce2
javierln <javier.lanza@semicrol.com>
2024-08-23 17:37:55 +0200

    Logout implemented

---
+++ BlazorWasmBlogClient/Pages/Authentication/Logout.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazorWasmBlogClient.Services.IServices;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWasmBlogClient.Pages.Authentication
+{
+    public partial class Logout
+    {
+        [Inject]
+        public IAuthService AuthService { get; set; }
+        [Inject]
+        public NavigationManager NavManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await AuthService.Exit();
+            NavManager.NavigateTo("/");
+        }
+
+    }
+}
