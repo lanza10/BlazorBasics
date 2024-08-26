e1c27d24a391e338f54e87a74f29677f0dfb69f0
javierln <javier.lanza@semicrol.com>
2024-08-26 10:15:31 +0200

    Radzen on webassembly

--- BlazorWasmBlogClient/Program.cs
+++ BlazorWasmBlogClient/Program.cs
@@ -5,6 +5,7 @@ using BlazorWasmBlogClient.Services.IServices;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +13,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Radzen
+builder.Services.AddRadzenComponents();
 
 //Add services
 builder.Services.AddScoped<IPostService, PostService>();
